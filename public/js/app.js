219d16b01b8ff097b160c70637b0b3bacf5c5ebc
Omar Ramadan <ramomar1992@outlook.com>
2021-04-12 23:10:51 +0300

    finished corona reports

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-const dataRangeByCountry = {
-    name: '',
-    dates: [],
-    deaths: [],
-    recovered: [],
-    active: [],
-    confirmed: []
-}
 const dataTodayByCountry = {
     name: '',
     deaths: '',
@@ -36,25 +28,37 @@ $(function () {
     $('#submitCorona').on('click', (evt) => {
         evt.preventDefault();
         const selectOption = $('#cnty').val();
-        if (selectOption)
-            $('canvas').remove();
-        dataRangeByCountry.name, dataTodayByCountry.name = selectOption;
-        const dateFrom = $('#dateFrom').val();
-        const dateTo = $('#dateTo').val();
+        if (selectOption) {
+            const dataRangeByCountry = {
+                name: '',
+                dates: [],
+                deaths: [],
+                recovered: [],
+                active: [],
+                confirmed: []
+            }
+            $('.todaysCoronaReport').html('');
+            $('.rangeCoronaReport').html('');
 
-        $.ajax({
-            url: `https://api.quarantine.country/api/v1/summary/latest`,
-            success: function (data1) {
-                let extractedData = Object.entries(data1.data.regions);
-                $('.todaysCoronaReport').html('').prepend('<canvas id="coronaTodayCanvas" width="400px" height="400px"></canvas>')
-                let canvas2 = document.getElementById('coronaTodayCanvas')
-                let context2 = canvas2.getContext('2d');
-                let targetData = extractedData.filter(elem => elem[0] == selectOption);
-                console.log(targetData);
-                dataTodayByCountry.deaths = newData[0][1].NewDeaths;
-                dataTodayByCountry.recovered = newData[0].NewRecovered;
-                dataTodayByCountry.confirmed = newData[0].NewConfirmed;
-                $('.todaysCoronaReport').append(`
+            dataRangeByCountry.name = selectOption;
+            dataTodayByCountry.name = selectOption;
+            const dateFrom = $('#dateFrom').val();
+            const dateTo = $('#dateTo').val();
+
+            $.ajax({
+                url: `https://api.quarantine.country/api/v1/summary/latest`,
+                success: function (data1) {
+                    let extractedData = Object.entries(data1.data.regions);
+                    $('.todaysCoronaReport').html('').prepend('<canvas id="coronaTodayCanvas" width="400px" height="400px"></canvas>')
+                    let canvas2 = document.getElementById('coronaTodayCanvas')
+                    let context2 = canvas2.getContext('2d');
+                    let targetData = extractedData.filter(elem => elem[0] == selectOption);
+                    dataTodayByCountry.deaths = targetData[0][1].change.deaths;
+                    dataTodayByCountry.recovered = targetData[0][1].change.recovered;
+                    dataTodayByCountry.active = targetData[0][1].change.active_cases;
+
+                    dataTodayByCountry.confirmed = targetData[0][1].change.total_cases;
+                    $('.todaysCoronaReport').append(`
                 <table>
                 <tr>
                     <td id="todayRecovered">
@@ -69,118 +73,126 @@ $(function () {
                         Confirmed: ${dataTodayByCountry.confirmed}
                     </td>
                     <td>
+                        Active:  ${dataTodayByCountry.active}
                     </td>
                 </tr>
                 </table>`);
-                console.log(dataTodayByCountry);
-                let myChart2 = new Chart(context2,
-
-                    {
-                        type: 'bar',
-                        data:
+                    let myChart2 = new Chart(context2,
 
                         {
-                            labels: [`${dataTodayByCountry.name.toUpperCase()} today`],
-                            datasets:
+                            type: 'bar',
+                            data:
 
-                                [{
-                                        label: 'Confirmed Casses',
-                                        data: [dataTodayByCountry.confirmed],
-                                        backgroundColor: 'rgba(0,0,200,0.3)',
-                                        borderWidth: 1
-                                    },
-                                    {
-                                        label: 'Deaths',
-                                        data: [dataTodayByCountry.deaths],
-                                        backgroundColor: 'rgba(200,0,0,0.3)',
-                                        borderWidth: 1
-                                    },
-                                    {
-                                        label: 'Recovered',
-                                        data: [dataTodayByCountry.recovered],
-                                        backgroundColor: 'rgba(0,200,0,0.3)',
-                                        borderWidth: 1
+                            {
+                                labels: [`${dataTodayByCountry.name.toUpperCase()} today`],
+                                datasets:
+
+                                    [{
+                                            label: 'Confirmed Casses',
+                                            data: [dataTodayByCountry.confirmed],
+                                            backgroundColor: 'rgba(0,0,200,0.3)',
+                                            borderWidth: 1
+                                        },
+                                        {
+                                            label: 'Deaths',
+                                            data: [dataTodayByCountry.deaths],
+                                            backgroundColor: 'rgba(200,0,0,0.3)',
+                                            borderWidth: 1
+                                        },
+                                        {
+                                            label: 'Recovered',
+                                            data: [dataTodayByCountry.recovered],
+                                            backgroundColor: 'rgba(0,200,0,0.3)',
+                                            borderWidth: 1
+                                        }, {
+                                            label: 'Active',
+                                            data: [dataTodayByCountry.active],
+                                            backgroundColor: 'rgba(175,200,0,0.3)',
+                                            borderWidth: 1
+                                        }
+                                    ]
+                            }
+
+                            ,
+                            options: {
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
                                     }
-                                ]
-                        }
-
-                        ,
-                        options: {
-                            scales: {
-                                y: {
-                                    beginAtZero: true
                                 }
                             }
                         }
-                    }
-                );
-            }
-        }).catch(err => console.log(err));
-        $.ajax({
-            url: `https://api.covid19api.com/total/country/${selectOption}?from=${dateFrom}&to=${dateTo}`,
-            success: function (data) {
-                $('.rangeCoronaReport').append('<canvas id="coronaTotalCanvas" width="400px" height="400px"></canvas>')
-                let canvas = document.getElementById('coronaTotalCanvas')
-                let context = canvas.getContext('2d');
+                    );
+                }
+            }).catch(err => console.log(err));
+            $.ajax({
+                url: `https://api.covid19api.com/total/country/${selectOption}?from=${dateFrom}&to=${dateTo}`,
+                success: function (data) {
 
-                data.forEach(elem => {
-                    if (elem.Date != "2021-03-07T00:00:00Z") {
-                        dataRangeByCountry.dates.push(elem.Date.slice(0, 10));
-                        dataRangeByCountry.deaths.push(elem.Deaths);
-                        dataRangeByCountry.recovered.push(elem.Recovered);
-                        dataRangeByCountry.confirmed.push(elem.Confirmed);
-                        dataRangeByCountry.active.push(elem.Active);
-                    }
-                });
-                let myChart = new Chart(context,
+                    $('.rangeCoronaReport').append('<canvas id="coronaTotalCanvas" width="400px" height="400px"></canvas>')
+                    let canvas = document.getElementById('coronaTotalCanvas')
+                    let context = canvas.getContext('2d');
 
-                    {
-                        type: 'line',
-                        data:
+                    data.forEach(elem => {
+                        if (elem.Date != "2021-03-07T00:00:00Z") {
+                            dataRangeByCountry.dates.push(elem.Date.slice(0, 10));
+                            dataRangeByCountry.deaths.push(elem.Deaths);
+                            dataRangeByCountry.recovered.push(elem.Recovered);
+                            dataRangeByCountry.confirmed.push(elem.Confirmed);
+                            dataRangeByCountry.active.push(elem.Active);
+                        }
+                    });
+                    console.log(dataRangeByCountry);
+                    let myChart = new Chart(context,
 
                         {
-                            labels: dataRangeByCountry.dates,
-                            datasets:
+                            type: 'line',
+                            data:
 
-                                [{
-                                        label: 'Confirmed Casses',
-                                        data: dataRangeByCountry.confirmed,
-                                        backgroundColor: 'rgba(0,0,200,0.3)',
-                                        borderWidth: 1
-                                    },
-                                    {
-                                        label: 'Active Cases',
-                                        data: dataRangeByCountry.active,
-                                        backgroundColor: 'rgba(200,200,0,0.3)',
-                                        borderWidth: 1
-                                    },
-                                    {
-                                        label: 'Deaths',
-                                        data: dataRangeByCountry.deaths,
-                                        backgroundColor: 'rgba(200,0,0,0.3)',
-                                        borderWidth: 1
-                                    },
-                                    {
-                                        label: 'Recovered',
-                                        data: dataRangeByCountry.recovered,
-                                        backgroundColor: 'rgba(0,200,0,0.3)',
-                                        borderWidth: 1
+                            {
+                                labels: dataRangeByCountry.dates,
+                                datasets:
+
+                                    [{
+                                            label: 'Confirmed Casses',
+                                            data: dataRangeByCountry.confirmed,
+                                            backgroundColor: 'rgba(0,0,200,0.3)',
+                                            borderWidth: 1
+                                        },
+                                        {
+                                            label: 'Active Cases',
+                                            data: dataRangeByCountry.active,
+                                            backgroundColor: 'rgba(200,200,0,0.3)',
+                                            borderWidth: 1
+                                        },
+                                        {
+                                            label: 'Deaths',
+                                            data: dataRangeByCountry.deaths,
+                                            backgroundColor: 'rgba(200,0,0,0.3)',
+                                            borderWidth: 1
+                                        },
+                                        {
+                                            label: 'Recovered',
+                                            data: dataRangeByCountry.recovered,
+                                            backgroundColor: 'rgba(0,200,0,0.3)',
+                                            borderWidth: 1
+                                        }
+                                    ]
+                            }
+
+                            ,
+                            options: {
+                                scales: {
+                                    y: {
+                                        beginAtZero: true
                                     }
-                                ]
-                        }
-
-                        ,
-                        options: {
-                            scales: {
-                                y: {
-                                    beginAtZero: true
                                 }
                             }
                         }
-                    }
-                );
-            }
+                    );
+                }
 
-        });
+            });
+        }
     });
-})
+});
